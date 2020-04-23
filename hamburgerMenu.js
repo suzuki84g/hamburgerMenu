e42f0287ab1af5c6336adec0dfed8ba78e36b4bf
suzuki84g <suzuki.84g@gmail.com>
2020-04-23 15:11:42 +0900

    方針

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,1 +1,8 @@
-init
+// 現状、ボタンアイコンの実装がなく、押下後のnavリストがいきなり表示されるので、idの付与をjsで操作してビジュアルをやっていく形にする。
+
+document.getElementById('nav-toggle');  // button要素の取得
+document.getElementById('nav-list');  // 表示されるリスト項目
+
+// #TODO:button押下時の動作
+// #TODO:button要素のクラス（＝buttonそのものをtoggleで切り替える）
+// #TODO:リスト要素のクラスをtoggleで切り替える
