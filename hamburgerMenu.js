f7446f855ede7e7f985443aa014950eabaff1f60
suzuki84g <suzuki.84g@gmail.com>
2020-04-23 15:46:11 +0900

    コメント修正

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,13 +1,14 @@
 // 現状、ボタンアイコンの実装がなく、押下後のnavリストがいきなり表示されるので、idの付与をjsで操作してビジュアルをやっていく形にする。
 
-const button = document.getElementById('nav-toggle');  // button要素を明示
 
 function btn_click() {
+    // button押下時の動作を記述
     window.alert('ボタンがクリックされました。');
-}
+};
 
-document.getElementById('nav-list');  // 表示されるリスト項目
+const button = document.getElementById('nav-toggle');  // button要素
+const navList = document.getElementById('nav-list');  // リスト要素 #WIP:liを含む形で記述が必要
 
-// #TODO:button押下時の動作
+// #TODO:button押下時の正しい動作
 // #TODO:button要素のクラス（＝buttonそのものをtoggleで切り替える）
 // #TODO:リスト要素のクラスをtoggleで切り替える
