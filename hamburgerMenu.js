ecf948df8e7386714855d305389c0374cb679341
suzuki84g <suzuki.84g@gmail.com>
2020-04-24 14:02:20 +0900

    toggleによるicon切り替え

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,10 +1,12 @@
 // 現状、ボタンアイコンの実装がなく、押下後のnavリストがいきなり表示されるので、idの付与をjsで操作してビジュアルをやっていく形にする。
 
 const button = document.getElementById('nav-toggle');  // button要素
+const icon = document.getElementById('hamburger-icon');  // icon
 const navList = document.getElementById('nav-list');  // nav-listを取得
 
 function btn_click() {
     // button押下時の動作を記述
     navList.classList.toggle('hidden');  // `hidden`classをtoggleで切り替える
-    // TODO:アイコンを変更する記載
+    icon.classList.toggle('fa-bars');  // 初期ON
+    icon.classList.toggle('fa-times');  // 初期OFF
 };
