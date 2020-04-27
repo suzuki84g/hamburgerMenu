7b81fb6b7f5f6b19a7894228ef31b91f9652a94b
suzuki84g <suzuki.84g@gmail.com>
2020-04-27 15:34:03 +0900

    修正

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -4,7 +4,7 @@ const button = document.getElementById('nav-toggle');  // button要素
 const icon = document.getElementById('hamburger-icon');  // icon
 const navList = document.getElementById('nav-list');  // nav-listを取得
 
-function btn_click() {
+button.onclick = () => {
     // button押下時の動作を記述
     navList.classList.toggle('hidden');  // `hidden`classをtoggleで切り替える
     icon.classList.toggle('fa-bars');  // 初期ON
